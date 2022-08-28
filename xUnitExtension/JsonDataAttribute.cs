fcc9e03e2acbf2a6b9d05f93319789811a6b1823
Hiroyoshi Uchiyama <hiroyoshi.uchiyama@aimnext.com>
2022-08-28 17:25:15 +0900

    xUnitExtension.JsonDataAttribute initial created

---
+++ xUnitExtension/JsonDataAttribute.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit.Sdk;
+
+namespace xUnitExtension
+{
+    public class JsonDataAttribute : DataAttribute
+    {
+        private readonly object[] _inlineData;
+        private readonly string? _key;
+        private string? _fileName;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JsonDataAttribute" /> class with default key (=same as method name)
+        ///     definition.
+        /// </summary>
+        public JsonDataAttribute()
+        {
+            _inlineData = Array.Empty<object>();
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JsonDataAttribute" /> class with key definition.
+        /// </summary>
+        public JsonDataAttribute(string key)
+        {
+            _inlineData = Array.Empty<object>();
+            _key = string.IsNullOrWhiteSpace(key) ? throw new ArgumentNullException(nameof(key)) : key;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JsonDataAttribute" /> class with inline data definitions.
+        /// </summary>
+        /// <param name="data">The inline data values to pass to the theory.</param>
+        public JsonDataAttribute(params object[] data)
+        {
+            _inlineData = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JsonDataAttribute" /> class with inline data definitions.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="data">The inline data values to pass to the theory.</param>
+        public JsonDataAttribute(string key, params object[] data)
+        {
+            _key = key ?? throw new ArgumentNullException(nameof(key));
+            _inlineData = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        public string? FileName
+        {
+            get => _fileName;
+            set => _fileName = string.IsNullOrWhiteSpace(value) ? null : value;
+        }
+
+        public override IEnumerable<object?[]> GetData(MethodInfo testMethod)
+        {
+            string key = _key ?? testMethod.Name;
+            using var stream = GetStream(testMethod.DeclaringType ?? throw new ArgumentException("DeclaringType is null", nameof(testMethod)));
+            using var streamReader = new StreamReader(stream);
+            using var jsonReader = new JsonTextReader(streamReader);
+            var rootJToken = JToken.Load(jsonReader)[key];
+            if (rootJToken == null)
+            {
+                throw new InvalidOperationException($"{key} was not found in the JSON root.");
+            }
+
+            object?[] PopulateTestMethodParameters(IEnumerable<ParameterInfo> parameters, JToken jObject) =>
+                _inlineData.Concat(from parameter in parameters.Skip(_inlineData.Length)
+                    join jProperty in jObject.Children().OfType<JProperty>() on parameter.Name equals jProperty.Name into gj
+                    from jProperty in gj.DefaultIfEmpty()
+                    select jProperty?.Value.ToObject(parameter.ParameterType)).ToArray();
+
+            return rootJToken switch
+            {
+                JArray jArray => jArray.Children().OfType<JObject>().Select(x => PopulateTestMethodParameters(testMethod.GetParameters(), x)),
+                JObject jObject => new []{PopulateTestMethodParameters(testMethod.GetParameters(), jObject)},
+                _ => Enumerable.Empty<object?[]>()
+            };
+        }
+
+        private Stream GetStream(Type declaringType)
+        {
+            if (declaringType.FullName == null) throw new ArgumentException("FullName is null", nameof(declaringType));
+            
+            Stream? TryToLoadFromResource()
+            {
+                var resourceName = FileName ?? declaringType.FullName;
+                return declaringType.Assembly.GetManifestResourceNames().Any(x => x == resourceName)
+                    ? declaringType.Assembly.GetManifestResourceStream(resourceName) : null;
+            }
+
+            string GetFileName() =>
+                FileName != null
+                    ? Path.HasExtension(FileName) ? FileName : $"{FileName}.json"
+                    : $"{declaringType.FullName}.json";
+
+            return TryToLoadFromResource() ?? File.OpenRead(GetFileName());
+        }
+    }
+}
---
+++ xUnitExtensionTest/JsonDataAttributeTest.cs
@@ -0,0 +1,154 @@
+using System.Data;
+using Newtonsoft.Json.Linq;
+using xUnitExtension;
+
+namespace xUnitExtensionTest;
+
+public class JsonDataAttributeTest
+{
+    #region Resouce Finding Tests
+
+    [Theory]
+    [JsonData(FileName = "FileName.json")]
+    public void LoadFromFileTest(int index, string value)
+    {
+        Assert.Equal(1, index);
+        Assert.Equal("LoadFromFileTestValue", value);
+    }
+
+    [Theory]
+    [JsonData(FileName = "xUnitExtensionTest.ResourceName.json")]
+    public void LoadFromSpecifiedResourceTest(int index, string value)
+    {
+        Assert.Equal(1, index);
+        Assert.Equal("LoadFromSpecifiedResourceTestValue", value);
+    }
+
+    [Theory]
+    [JsonData]
+    public void LoadFromDefaultResourceTest(int index, string value)
+    {
+        Assert.Equal(1, index);
+        Assert.Equal("LoadFromDefaultResourceTestValue", value);
+    }
+
+    [Fact]
+    public void ResourceNotFoundTest()
+    {
+        // Arrange
+        var sut = new JsonDataAttribute() { FileName = "NotExistResource" };
+        var methodInfo = this.GetType().GetMethod(nameof(ResourceNotFoundTest))!;
+        // Act & Assert
+        Assert.Throws<FileNotFoundException>(() => sut.GetData(methodInfo));
+    }
+
+    [Theory]
+    [JsonData("KeyName")]
+    public void KeyTest(int index, string value)
+    {
+        Assert.Equal(1, index);
+        Assert.Equal("KeyTestValue", value);
+    }
+
+    [Fact]
+    public void KeyNotFoundTest()
+    {
+        // Arrange
+        var sut = new JsonDataAttribute("NotExistKeyName");
+        var methodInfo = this.GetType().GetMethod(nameof(ResourceNotFoundTest))!;
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => sut.GetData(methodInfo));
+    }
+
+    #endregion
+
+    [Theory]
+    [JsonData]
+    public void FromArrayTest(int index, string value)
+    {
+        Assert.Equal($"FromArrayTestValue{index}", value);
+    }
+
+    [Theory]
+    [JsonData]
+    public void FromObjectTest(int index, string value)
+    {
+        Assert.Equal(1, index);
+        Assert.Equal("FromObjectTestValue", value);
+    }
+
+    [Theory]
+    [JsonData]
+    public void VariousParameterTypesTest(string param1, long param2, dynamic param3,
+        TestData testData, DataTable dataTable,
+        JArray jArray, JObject jObject, JValue jValue)
+    {
+        var expectedTestData = new TestData()
+        {
+            IntProperty = 3421,
+            StringProperty = "TestDataStringPropertyValue"
+        };
+
+        Assert.Equal("param1", param1);
+
+        Assert.Equal(1234567890123456789L, param2);
+
+        Assert.Equal(541, (int)param3.IntProperty.Value);
+        Assert.Equal("StringPropertyValue", (string)param3.StringProperty.Value);
+
+        Assert.Equal(expectedTestData, testData, TestData.Comparer);
+
+        Assert.Equal("Index", dataTable.Columns[0].ColumnName);
+        Assert.Equal("Column", dataTable.Columns[1].ColumnName);
+        Assert.All(dataTable.AsEnumerable(), (row, i) =>
+        {
+            Assert.Equal(i + 1L, row.ItemArray[0]);
+            Assert.Equal($"Value{i + 1}", row.ItemArray[1]);
+        });
+
+        Assert.Equal(10, jArray[0].Value<int>());
+        Assert.Equal("Value20", jArray[1].Value<string>());
+
+        Assert.Equal(100, jObject["Property1"]?.Value<int>());
+        Assert.Equal("Value200", jObject["Property2"]?.Value<string>());
+
+        Assert.Equal("jValueValue", jValue.Value<string>());
+    }
+
+    [Theory]
+    [JsonData(1, "FromInlineData1")]
+    [JsonData(2, "FromInlineData2")]
+    public void InlineDataTest(int index, string value, int fromJsonIndex, string fromJsonValue)
+    {
+        Assert.Equal($"FromInlineData{index}", value);
+        Assert.Equal(101, fromJsonIndex);
+        Assert.Equal("InlineDataTestValue", fromJsonValue);
+    }
+
+    public class TestData
+    {
+        private sealed class TestDataEqualityComparer : IEqualityComparer<TestData>
+        {
+            public bool Equals(TestData x, TestData y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.IntProperty == y.IntProperty && x.StringProperty == y.StringProperty &&
+                       x.UnspecifiedProperty == y.UnspecifiedProperty;
+            }
+
+            public int GetHashCode(TestData obj)
+            {
+                return HashCode.Combine(obj.IntProperty, obj.StringProperty, obj.UnspecifiedProperty);
+            }
+        }
+
+        public static IEqualityComparer<TestData> Comparer { get; } = new TestDataEqualityComparer();
+
+        public int IntProperty { get; set; }
+        public string? StringProperty { get; set; }
+        public long? UnspecifiedProperty { get; set; }
+    }
+}
